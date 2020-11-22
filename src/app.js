ac974fa73b5750fcd3f1a16e1e45c7d7107af6ef
Alen <1697260287@qq.com>
2020-11-22 12:48:27 +0800

    feat: 完善404和错误页面

--- src/app.js
+++ src/app.js
@@ -6,11 +6,21 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const { isProd } = require('./utils/env')
+
+// 路由
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onErrorConf = {}
+if (isProd) {
+  onErrorConf = {
+    redirect: '/error'
+  }
+}
+onerror(app, onErrorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -24,17 +34,10 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
