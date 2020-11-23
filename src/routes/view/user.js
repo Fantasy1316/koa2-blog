ae92f905f3c62f82327f144c9520e02963822573
Alen <1697260287@qq.com>
2020-11-23 17:17:12 +0800

    fix: 判断用户登录页面信息更改

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,14 +5,34 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取用户登录信息
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false //默认未登录
+  }
+
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+
+  return data
+}
+
 // login 登陆
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 // register 注册
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
